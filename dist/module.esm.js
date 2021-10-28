4135bb4dc6322faa6a0ed275e588c79c28266bbc
Ryan Chandler <ryangjchandler@gmail.com>
2021-10-28 13:20:33 +0100

    refactor: reusable function for modifier arguments

--- dist/module.esm.js
+++ dist/module.esm.js
@@ -3352,16 +3352,19 @@ var buildConfigFromModifiers = (modifiers) => {
   const config = {
     plugins: []
   };
+  const getModifierArgument = (modifier) => {
+    return modifiers[modifiers.indexOf(modifier) + 1];
+  };
   if (modifiers.includes("duration")) {
-    config.duration = parseInt(modifiers[modifiers.indexOf("duration") + 1]);
+    config.duration = parseInt(getModifierArgument("duration"));
   }
   if (modifiers.includes("delay")) {
-    const delay = modifiers[modifiers.indexOf("delay") + 1];
+    const delay = getModifierArgument("delay");
     config.delay = delay.includes("-") ? delay.split("-").map((n) => parseInt(n)) : parseInt(delay);
   }
   if (modifiers.includes("cursor")) {
     config.plugins.push(import_tippy.followCursor);
-    const next = modifiers[modifiers.indexOf("cursor") + 1];
+    const next = getModifierArgument("cursor");
     if (["x", "initial"].includes(next)) {
       config.followCursor = next === "x" ? "horizontal" : "initial";
     } else {
@@ -3369,7 +3372,7 @@ var buildConfigFromModifiers = (modifiers) => {
     }
   }
   if (modifiers.includes("on")) {
-    config.trigger = modifiers[modifiers.indexOf("on") + 1];
+    config.trigger = getModifierArgument("on");
   }
   if (modifiers.includes("arrowless")) {
     config.arrow = false;
@@ -3381,19 +3384,19 @@ var buildConfigFromModifiers = (modifiers) => {
     config.interactive = true;
   }
   if (modifiers.includes("border") && config.interactive) {
-    config.interactiveBorder = parseInt(modifiers[modifiers.indexOf("border") + 1]);
+    config.interactiveBorder = parseInt(getModifierArgument("border"));
   }
   if (modifiers.includes("debounce") && config.interactive) {
-    config.interactiveDebounce = parseInt(modifiers[modifiers.indexOf("debounce") + 1]);
+    config.interactiveDebounce = parseInt(getModifierArgument("debounce"));
   }
   if (modifiers.includes("max-width")) {
-    config.maxWidth = parseInt(modifiers[modifiers.indexOf("max-width") + 1]);
+    config.maxWidth = parseInt(getModifierArgument("max-width"));
   }
   if (modifiers.includes("theme")) {
-    config.theme = modifiers[modifiers.indexOf("theme") + 1];
+    config.theme = getModifierArgument("theme");
   }
   if (modifiers.includes("placement")) {
-    config.placement = modifiers[modifiers.indexOf("placement") + 1];
+    config.placement = getModifierArgument("placement");
   }
   return config;
 };
