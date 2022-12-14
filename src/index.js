02eac8dc6d6651142a57b1b1e7d83ef89f3bf48d
Felix Lebel <felix@vynatu.io>
2022-12-14 11:55:11 -0500

    Use delay instead of css transition timings
    ... and let their hooks take care of destroying the instance.
    
    I am posting the code on the upstream (here) instead of pulling it into filament. If it is approved I'll amend my commit (I really should have made a PR here at first).
    
    This is for my [pull request for filament](https://github.com/filamentphp/filament/pull/5254)

--- src/index.js
+++ src/index.js
@@ -2,25 +2,46 @@ import tippy from 'tippy.js'
 import { buildConfigFromModifiers } from './buildConfigFromModifiers'
 
 export default function (Alpine) {
-    Alpine.magic('tooltip', (el) => {
+    Alpine.magic('tooltip', el => {
         return (content, config = {}) => {
+            const onHiddenCallback = config.hasOwnProperty('onHidden') ?
+                function (instance) {
+                    // The lifecycle hooks do not seem to care about the return
+                    // value of this hook. We will hijack it, and if it returns
+                    // false we will not destroy the instance.
+                    if (config.onHidden(instance) !== false)
+                        instance.destroy()
+                } : function (instance) {
+                    instance.destroy()
+                }
+
+            // Since delay doesn't do anything
+            // when we spawn it programatically,  
+            // we can use it to control the duration 
+            // of the tooltip.
+            const delay = (config.delay == null)
+                ? 300
+                : (Array.isArray(config.delay))
+                    ? config.delay[0] + config.delay[1]
+                    : (Number.isInteger(config.delay))
+                        ? config.delay
+                        : 300
+
             const instance = tippy(el, {
                 content,
                 trigger: 'manual',
-                ...config
+                ...config,
+                onHidden: onHiddenCallback,
+                delay
             })
 
             instance.show()
 
-            setTimeout(() => {
-                instance.hide()
-
-                setTimeout(() => instance.destroy(), config.duration || 300)
-            }, config.timeout || 2000)
+            setTimeout(() => instance.hide(), delay)
         }
     })
 
-    Alpine.directive('tooltip', (el, { modifiers, expression }, { evaluateLater, effect }) => {
+    Alpine.directive('tooltip', (el, {modifiers, expression}, {evaluateLater, effect}) => {
         const config = modifiers.length > 0
             ? buildConfigFromModifiers(modifiers)
             : {}
